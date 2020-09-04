bf7edd0602f91e5c7ae176420fe438d8fa766a29
Vincenzo <megnavincenzo97@gmail.com>
2020-09-04 07:44:57 +0200

    Header e Css

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <title>Google-faq</title>
+  </head>
+  <body>
+
+  <header>
+    <div class="header-top">
+      <h2><img src="img/logo.png" alt="logo-google"> Privacy e termini</h2>
+    </div>
+    <div class="header-bottom">
+      <div class="header-bottom-left left">
+        <ul class="list-inline">
+          <li><a href="#">Introduzione</a></li>
+          <li><a href="#">Norme sulla privacy</a></li>
+          <li><a href="#">Termini di servizio</a></li>
+          <li><a href="#">Tecnologie</a></li>
+          <li><a class="active" href="#">Domande frequenti</a></li>
+        </ul>
+      </div>
+      <div class="header-bottom-right right">
+        <ul class="list-inline">
+          <li><a href="#">Account Google</a></li>
+        </ul>
+      </div>
+    </div>
+  </header>
+
+  </body>
+</html>
